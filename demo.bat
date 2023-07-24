1d10427570c5ae488e5fe3962f75f72a57308d2b
sejalsurse <71496014+sejalsurse@users.noreply.github.com>
2023-07-24 06:38:50 -0500

    Update demo.bat

--- demo.bat
+++ demo.bat
@@ -1,5 +1,5 @@
 echo "Hello World! This is freestyle demo project."
 C:\MinGW\bin\g++.exe --version
 dir
-C:\MinGW\bin\g++.exe hello_world.cpp
-a
+C:\MinGW\bin\g++.exe hello_world.cpp -o hello_world.exe
+hello_world.exe
